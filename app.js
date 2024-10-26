26398b5d06a31a4f28f3993bace6867f48ec7495
krisnaepras <krisnaepras@gmail.com>
2024-10-26 13:38:10 +0700

    last

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const path = require("path");
+
 const userRoutes = require("./routes/userRoutes");
 const accountRoutes = require("./routes/accountRoutes");
 const transactionRoutes = require("./routes/transactionRoutes");
 
+const swaggerDocument = YAML.load(path.join(__dirname, "./docs/swagger.yml"));
+
 const app = express();
 app.use(bodyParser.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/accounts", accountRoutes);
